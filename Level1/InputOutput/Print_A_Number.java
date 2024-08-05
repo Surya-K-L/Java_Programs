d91d565256705ecfde2c3fc6ce9a9582c8195e23
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-08-05 09:04:16 +0530

    Create Print_A_Number.java

---
+++ Level1/InputOutput/Print_A_Number.java
@@ -0,0 +1,15 @@
+// Write a program to get a number and print the number
+// For example:
+
+// Test	       Input	Result
+// Test Case 1     6        6
+
+import java.util.*;
+public class Print_A_Number {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(n);
+        sc.close();
+    }
+}
