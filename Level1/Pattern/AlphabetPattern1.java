f11c52e44fd02bca0dfdff2d12aed4aa5f85cf2b
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-08-06 06:25:58 +0530

    Update AlphabetPattern1.java

--- Level1/Pattern/AlphabetPattern1.java
+++ Level1/Pattern/AlphabetPattern1.java
@@ -1,1 +1,31 @@
+// Print the given square pattern
 
+// For example:
+
+// Input	Result
+// 2       A B
+//         A B 
+
+// 3       A B C 
+//         A B C 
+//         A B C 
+
+
+
+import java.util.*;
+public class AlphabetPattern1{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print((char)(j+65)+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
