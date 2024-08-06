08ad021fd9c772e17c11a2c553a989dd4cf2b27e
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-08-06 06:29:21 +0530

    StarPattern

---
+++ Level1/Pattern/StarPattern1.java
@@ -0,0 +1,27 @@
+// Print the given pattern
+
+// For example:
+
+// Input Result 
+// 2     * * 
+//       * *
+
+// 3     * * *
+//       * * *
+//       * * *
+
+import java.util.*;
+
+public class StarPattern1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
