c0bc51f0713f16e4e67e0c484fdbeecf427ad867
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-08-14 06:00:30 +0530

    Print_Alternative

---
+++ Level2/Modules/Print_Alternatively.java
@@ -0,0 +1,31 @@
+// For a given set of numbers print alternatively one element from the first and one element from the last
+
+// For this question, you need to have an array - it is not possible just with loops
+
+// For example:
+
+// Input	               Result
+// 6                       1 6 2 5 3 4
+// 1 2 3 4 5 6
+
+// 6                       10 60 20 50 30 40
+// 10 20 30 40 50 60
+
+import java.util.*;
+public class Print_Alternatively{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n],t=n-1;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n/2;i++)
+        {
+            System.out.print(a[i]+" "+a[t--]+" ");
+        }
+        sc.close();
+    }
+}
