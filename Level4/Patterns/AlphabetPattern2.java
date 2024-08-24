0aa5218efc7969375d591f06958c2c42444e94ea
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-08-24 06:06:25 +0530

    Alphabet_Pattern2

---
+++ Level4/Patterns/AlphabetPattern2.java
@@ -0,0 +1,55 @@
+// Print following pattern for given value of N
+
+// Input
+// 7
+// Output
+// ABCDEFGGFEDCBA
+// ABCDEF  FEDCBA
+// ABCDE    EDCBA
+// ABCD      DCBA
+// ABC        CBA
+// AB          BA
+// A            A
+
+// For example:
+
+// Input	Result
+// 7
+// ABCDEFGGFEDCBA
+// ABCDEF  FEDCBA
+// ABCDE    EDCBA
+// ABCD      DCBA
+// ABC        CBA
+// AB          BA
+// A            A
+
+
+import java.util.*;
+public class AlphabetPattern2{
+    public static void main(String h[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),t=65,m=64;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=n;j>=i;j--)
+            {
+                System.out.print((char)(t));
+                t++;
+            }
+            for(int k=1;k<i;k++)
+            {
+                System.out.print("  ");
+            }
+            for(int l=n;l>=i;l--)
+            {
+                System.out.print((char)(l+m));
+                
+            }
+            t=65;
+            m=m-1;
+            System.out.println();
+        }
+        sc.close();
+    }
+}
