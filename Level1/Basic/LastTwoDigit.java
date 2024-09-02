6653a291c5db0e7636e8ee215470ff93b6885af7
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-02 21:07:50 +0530

    Create LastTwoDigit.java

---
+++ Level1/Basic/LastTwoDigit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class LastTwoDigit{
+    public static void main(String h[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int t=n%100;
+        System.out.print(t);
+    }
+}
