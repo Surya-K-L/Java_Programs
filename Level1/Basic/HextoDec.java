ef53642469076c3306b3df6436adad02efd62b16
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-02 21:06:25 +0530

    Create  HextoDec.java

---
+++ Level1/Basic/HextoDec.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class HextoDec
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(16);
+        System.out.print(n);
+    }
+}
