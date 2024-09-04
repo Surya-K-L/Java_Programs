7664f837106d529d248226ffa4e8cc9216cc5f76
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-04 21:42:31 +0530

    Create MaxOfTwoNum.java

---
+++ Level1/Basic/MaxOfTwoNum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MaxOfTwoNum{
+    public static void main(String h[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        if(n>m)
+        System.out.print(n +" is greatest");
+        else
+        System.out.print(m+" is greatest");
+    }
+}
