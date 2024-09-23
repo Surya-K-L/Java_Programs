46d3af4e2be90a6478a9a17c13dc662ced54a6d3
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-23 22:14:10 +0530

    Create jdbc.java

---
+++ JDBC/jdbc.java
@@ -0,0 +1,21 @@
+package defaultpackage;
+import java.sql.*;
+public class jdbc {
+	public static void main(String args[])
+	{
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc","root","SuryaHr@74");
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery("Select * from books");
+			while(rs.next())
+			{
+				System.out.println(rs.getString(1)+"\t"+rs.getString(2));
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.print(e.toString());
+		}
+	}
+}
