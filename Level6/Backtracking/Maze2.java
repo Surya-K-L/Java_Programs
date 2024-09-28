1542fbdc8bc13222bf00c849b592df6f104f8fa9
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-28 21:13:23 +0530

    Create Maze2.java

---
+++ Level6/Backtracking/Maze2.java
@@ -0,0 +1,64 @@
+import java.util.*;
+public class Maze2{
+    public static void main(String h[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[][]=new int[n][n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                a[i][j]=sc.nextInt();
+            }
+        }
+        int sol[][]=new int[n][n];
+        if(Maze(a,0,0,sol))
+        {
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    System.out.print(sol[i][j]+" ");
+                }
+                System.out.println();
+            }
+        }
+        else
+        {
+            System.out.print("No solution exists");
+        }
+    }
+    public static boolean Maze(int a[][],int i,int j,int sol[][])
+    {
+        int n=sol.length;
+        if(i==n-1 && j==n-1 && a[i][j]==0)
+        {
+            sol[i][j]=1;
+            return true;
+        }
+        if(i>=0 && j>=0 && i<n && j<n && a[i][j]==0 && sol[i][j]!=1)
+        {
+            sol[i][j]=1;
+            if(Maze(a,i,j+1,sol))
+            {
+                return true;
+            }
+            if(Maze(a,i+1,j,sol))
+            {
+                return true;
+            }
+            if(Maze(a,i-1,j,sol))
+            {
+                return true;
+            }
+            if(Maze(a,i,j-1,sol))
+            {
+                return true;
+            }
+            sol[i][j]=0;
+            return false;
+        }
+        return false;
+    }
+}
